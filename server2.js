a3d17864b4d1b7603bc9407bb44e5c13e0e6c553
DongMyoungE <93035393+DongMyoungE@users.noreply.github.com>
2021-11-15 16:50:13 +0900

    Update server2.js

--- server2.js
+++ server2.js
@@ -16,24 +16,28 @@ app.use(logger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.listen(PORT, () => {
-    console.log(`✅OK. Server is Connected! app listening at http://localhsot:${PORT}`)
-});
-
 app.get("/:walletPubkeyStr([0-9a-zA-Z]{40,50})", async (req, res) => {
-    const tokenArrayWithMeta= async(tokenArray) => {
+    const tokenArrayWithMeta = async (tokenArray) => {
         const rawMetadata = await Promise.all(
             tokenArray.map((element) => getNft(element))
         );
         return rawMetadata;
     };
-
-
     const connection = new Connection("https://api.devnet.solana.com/", "confirmed")
     const { walletPubkeyStr } = req.params;
     const walletParsedInfo = await connection.getParsedTokenAccountsByOwner(new PublicKey(walletPubkeyStr), { programId: TOKEN_PROGRAM_ID }, "confirmed");
-    const tokenArray = walletParsedInfo.value.filter((element)=> element.account.data.parsed.info.tokenAmount.amount === "1").map(
+    const tokenArray = walletParsedInfo.value.filter((element) => element.account.data.parsed.info.tokenAmount.amount === "1").map(
         (element) => element.account.data.parsed.info.mint
     );
     res.json((await tokenArrayWithMeta(tokenArray)))
 })
+
+app.get("/image/:mintPubkeyStr([0-9a-zA-Z]{40,50})", async (req, res) => {
+    const { mintPubkeyStr } = req.params;
+    const rawMetadata = await getNft(mintPubkeyStr);
+    res.json({ imageUrl: rawMetadata.image })
+});
+
+app.listen(PORT, () => {
+    console.log(`✅OK. Server is Connected! app listening at http://localhost:${PORT}`)
+});
